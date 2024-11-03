ab4b5a30104416d1e53450f18df4cbe09898f96f
kunman93 <kun.manu93@gmail.com>
2024-11-03 14:20:02 +0100

    feat: load a planet earth gltf model
    - replace cube with planet earth model

--- src/planet-earth.ts
+++ src/planet-earth.ts
@@ -1,59 +1,50 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-let renderer: any, camera: any, scene: any, cube: any, controls: any;
+let renderer: any, camera: any, scene: any, planetEarthModel: any, controls: any;
 
 /**
- *  ---- Cube animation in Three.js ---- 
+ *  ---- Planet Earth animation in Three.js ---- 
  */
 main();
 
 function main() {
   // # Initialising the canvas and the renderer
   const canvas = document.querySelector('#canvas') ?? undefined;
-  renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
+  renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true, canvas });
 
   // # Setting up the camera
   // ## Setting for the camera
   const fov = 75; // field of view, 75 degree
   const aspect = 2;  // it is the display aspect of the canvas, the default canvas is 300x150 pixel, which makes the aspect 300/150 or 2.
   // near and far represent the space in front of the the camera that will be rendered. Anything before or after that range will be clipped (not drawn)
-  const near = 0.1;
-  const far = 5;
+  const near = 5;
+  const far = 30;
 
   // ## Initalising the camera
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far); // Anything inside the defined frustum will be drawn. Anything outside will not.
-  camera.position.set(0, 0, 2); // Camera Coordinate System --> left-handed (index finger => x, thumb => y, middle finger => z)
+  camera.position.set(0, 0, 9); // Camera Coordinate System --> left-handed (index finger => x, thumb => y, middle finger => z)
 
   // # Setting up the scene
   scene = new THREE.Scene();
 
-  // # Creating a cube
-  // ## Creating a box geometry - the shape of the object -, which contains the data of the box
-  const boxWidth = 1;
-  const boxHeight = 1;
-  const boxDepth = 1;
-  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+  // # Load gltf model
+  const modelName = `planet_earth.glb`;
+  const gltfLoader = new GLTFLoader();
+  const url = `assets/${modelName}`;
+  gltfLoader.load(url, async function (gltf) {
+    planetEarthModel = gltf.scene;
 
-  // ## Creating a mesh phong material (shiny or flat, what color, what textures to apply, etc.) which is affected by the light
-  const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
-
-  // ## Creating a mesh
-  /**
-   * a mesh represents the combination of three things: 
-   * - Geometry
-   * - Material 
-   * - position, orientation, and scale of that object in the scene relative to its parent
-  */
-  cube = new THREE.Mesh(geometry, material);
-
-  // ## Add newly created cube to the scene
-  scene.add(cube);
+    // wait until the model can be added to the scene without blocking due to shader compilation
+    await renderer.compileAsync(planetEarthModel, camera, scene);
+    scene.add(planetEarthModel);
+  });
 
   // # Creating lights
   const color = 0xFFFFFF;
   const intensity = 3;
-  const lightPos = 1;
+  const lightPos = 100;
 
   const light1 = new THREE.DirectionalLight(color, intensity);
   light1.position.set(-lightPos, 0, 0);
@@ -91,9 +82,7 @@ function main() {
   controls.dampingFactor = 0.05; // The damping inertia used, default is 0.05
 
   controls.screenSpacePanning = false;
-
-  controls.minDistance = 1.5; // how far you can dolly in
-  controls.maxDistance = 3; // how far you can dolly out
+  controls.enableZoom = false;
   controls.target.set(0, 0, 0); // The focus point of the controls
 
   controls.maxPolarAngle = Math.PI; // How far you can orbit vertically, upper limit. Range is 0 to Math.PI radians, and default is Math.PI. 
@@ -104,9 +93,13 @@ function main() {
 }
 
 function animateRotation() {
-  cube.rotation.x += 0.02;
-  cube.rotation.y += 0.02;
-  cube.rotation.z += 0.02;
+  if (planetEarthModel) {
+    if (planetEarthModel.rotation.y > 2 * Math.PI) {
+      planetEarthModel.rotation.y = 0;
+    } else {
+      planetEarthModel.rotation.y += (Math.PI / 360);
+    }
+  }
 
   onWindowResize();
   controls.update();
